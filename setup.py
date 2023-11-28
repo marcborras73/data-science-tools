7b93b4d9c9926b4454aa7841ede68500ca01cbf8
marcborras73 <maboca.73@gmail.com>
2023-11-28 09:40:09 +0100

    adding setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="dstools",
+    packages=find_packages(),
+    version="0.1",
+    description="tools for data science module",
+    author="**MABOCA**",
+)
